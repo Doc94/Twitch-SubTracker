f1193f70782bd05f264765e1d52bd54bf4e59bac
Doc <nachito94@msn.com>
2021-01-23 21:47:43 -0300

    Se setean ID de formularios de recompensas

--- src/main/java/me/mrdoc/twitch/subtracker/classes/forms/TwitchRewardsForm.java
+++ src/main/java/me/mrdoc/twitch/subtracker/classes/forms/TwitchRewardsForm.java
@@ -3,10 +3,10 @@ package me.mrdoc.twitch.subtracker.classes.forms;
 import io.github.stepio.jgforms.question.MetaData;
 
 public enum TwitchRewardsForm implements MetaData {
-    NICK_REDEEMED(0L),
-    ID_REDEEMED(0L),
-    ID_REWARD(0L),
-    TITLE_REWARD(0L),
+    ID_REDEEMED(1438793387),
+    NICK_REDEEMED(1820502547),
+    ID_REWARD(2102415750),
+    TITLE_REWARD(1413301137),
     ;
 
     private long id;
