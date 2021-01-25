a6a42015e87c999590812f930660881fdab20651
Doc <nachito94@msn.com>
2021-01-25 15:01:40 -0300

    Update de entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ cd /usr/local/
 # Output Current Java Version
 java -version
 
-java -jar app.jar --twitch-token=${TWITCH_TOKEN} --twitch-channelId=${TWITCH_CHANNELID} --subsformId=${GOOGLE_FORM_ID_SUBS} --bitsformId=${GOOGLE_FORM_ID_BITS}
+java -jar app.jar --twitch-token=${TWITCH_TOKEN} --twitch-channelId=${TWITCH_CHANNELID} --rewardId=${TWITCH_REWARD_ID} --rewardsformId=${GOOGLE_FORM_ID_REWARDS} --subsformId=${GOOGLE_FORM_ID_SUBS} --bitsformId=${GOOGLE_FORM_ID_BITS}
